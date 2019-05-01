cc9d74a34880edc988bc3adbdd81b4971133daa5
Hejle <joachim-dk@hotmail.com>
2019-05-01 15:58:58 +0200

    Very Basic Composite Generator

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import runner
+from datetime import datetime
 from hypothesis import settings, given
-from hypothesis.strategies import just
-code = """fun main(args: Array<String>) {
-    println("Hello, World!")
-}
-"""
+from hypothesis.strategies import just, text, characters, composite
 
-@given(just(code))
+names = text(
+    characters(max_codepoint=1000, blacklist_categories=('Cc', 'Cs')),
+    min_size=1).map(lambda s: s.strip()).filter(lambda s: len(s) > 0)
+
+@composite
+def projects(draw):
+    name = draw(names)
+    code = """fun main(args: Array<String>) {
+        println("Hello, World!")
+    }
+    """
+    return code.replace("World", name)
+
+@given(projects())
 @settings(deadline=None)
 def compilertest(s):
-    output1 = runner.run(s, "kotlinc-jvm")
-    output2 = runner.run(s, "kotlinc")
+    dt = datetime.now()
+    name = "out/folder" + (str(dt.microsecond))
+    print("run")
+    output1 = runner.run(s, "kotlinc-jvm", outputDirectory=name)
+    output2 = runner.run(s, "kotlinc-native", outputDirectory= name + "-native")
     assert output1 == output2
 
 compilertest()
