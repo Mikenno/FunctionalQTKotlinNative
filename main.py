8b0164bc8864ed90b819b292f5e407d81f7823e0
fotoply <norberg.niels@gmail.com>
2019-05-09 11:42:11 +0200

    More variability

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import random
 import string
 import time
 
-
 names = text(characters(max_codepoint=150, whitelist_categories=('Lu', 'Ll')), min_size=3)
 numbers = integers()
 
 depth = 1
+
 
 @composite
 def projects(draw):
@@ -26,12 +26,13 @@ def genCode(draw):
     fuel = 25
     string_code = ""
     variables = []
-    while(fuel > 0):
+    while (fuel > 0):
         newCode, newVariableList = genExp(draw, variables)
         variables = newVariableList
         string_code += newCode
         fuel -= 1
     return string_code
+
 
 def genExp(draw, variables):
     r = random.randint(1, 2)
@@ -41,13 +42,48 @@ def genExp(draw, variables):
     elif r == 2:
         return genVariableChange(draw, variables)
 
+
+def genVariableOperator():
+    r = random.randint(1, 10)
+    if r == 1:
+        return "="
+    elif r == 2:
+        return "+="
+    elif r == 3:
+        return "-="
+    elif r == 4:
+        return "/="
+    elif r == 5:
+        return "*="
+    elif r == 6:
+        return "+"
+    elif r == 7:
+        return "-"
+    elif r == 8:
+        return "*"
+    elif r == 9:
+        return "/"
+    elif r == 10:
+        return "%"
+
+
+def genValue(draw, variables):
+    r = random.randint(1, 4)
+    if r < 4 or len(variables) == 0:
+        return draw(numbers)
+    else:
+        r = random.randint(0, len(variables) - 1)
+        return variables[r]
+
+
 def genVariableChange(draw, variables):
     if len(variables) == 0:
         return genVariable(draw, variables)
 
-    index = random.randint(0, len(variables))
-    variableName = variables[index-1]
-    return (depth*"\t" + variableName + "+=" + str(draw(numbers)) + ";\n"), variables
+    index = random.randint(0, len(variables) - 1)
+    variableName = variables[index]
+    return (depth * "\t" + variableName + genVariableOperator() + str(genValue(draw, variables)) + ";\n"), variables
+
 
 def genVariable(draw, variables):
     newName = True
@@ -57,12 +93,14 @@ def genVariable(draw, variables):
         if name not in variables:
             newName = False
             variables.append(name)
-    return (depth*"\t" + 'var ' + name + ' = ' + str(draw(numbers)) + ';\n'), variables
+    return (depth * "\t" + 'var ' + name + ' = ' + str(draw(numbers)) + ';\n'), variables
+
 
 def randomString(stringLength=10):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
+
 
 @composite
 def projectsv2(draw):
@@ -72,6 +110,7 @@ input}
     """
     return code.replace("input", gen)
 
+
 @given(projectsv2())
 @settings(deadline=None, suppress_health_check=HealthCheck.all(), max_examples=20)
 def compilertest(s):
@@ -79,8 +118,9 @@ def compilertest(s):
     name = "out/folder" + (str(dt.microsecond))
     print("run")
     output1 = runner.run(s, "kotlinc-jvm", outputDirectory=name)
-    output2 = runner.run(s, "kotlinc-native", outputDirectory= name + "-native")
+    output2 = runner.run(s, "kotlinc-native", outputDirectory=name + "-native")
     assert output1 == output2
     time.sleep(1)
-compilertest()
 
+
+compilertest()
