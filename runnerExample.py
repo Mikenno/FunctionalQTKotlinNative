c6853863f80c8cf4881ff705cb9b973193b12b18
fotoply <norberg.niels@gmail.com>
2019-05-16 13:29:42 +0200

    Broke stuff

--- runnerExample.py
+++ runnerExample.py
@@ -1,12 +1,12 @@
 import runner
 
-code = """fun main(args: Array<String>) {
-    println("Hello, World!")
-}"""
-output = runner.run(code)
+code = """fun main(args: Array<String>) {\n\tvar AAA = 0;\n\tvar AAB = 0;\n\tvar ABB = 0;\n\tvar BAA = 0;\n\tvar ABA = 0;\n\tvar AAAA = AAA;\n\tvar BBA = A
+AA;\n\tBBA=0;\n\tBAA=0;\n\tBBA=0;\n\tBAA=0;\n\tAAA=0;\n\tABB=0;\n\tABA=0;\n\tBAA=0;\n\tBAA=0;\n\tBBA=0;\n\tBBA=0;\n\tABB=0;\n\tBAA=0;\n\tABB=0;\n\tABA=0;\n\tAAB=AAA;\n\tABA=AAA;\n\tAAB=A
+AA;\n}\n    """
+output = runner.run(code,outputDirectory="out\\example")
 
-print(output[1])
+print(output[0])
 
-output = runner.run(code, "kotlinc-native")
+output = runner.run(code, "kotlinc-native", outputDirectory="out\\example")
 
-print(output[1])
+print(output[0])
